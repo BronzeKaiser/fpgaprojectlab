97588531f8866bdfc5ae5fd8ade894ca2b3ff812
James <36833665+BronzeKaiser@users.noreply.github.com>
2024-02-25 02:29:45 -0600

    Update motorcontrol.v

--- motor/motorcontrol.v
+++ motor/motorcontrol.v
@@ -1,14 +1,17 @@
 module motorcontrol(
     input clk,
-    input [3:0] sw,
+    input btnU,
+    input btnD,
+    input btnL,
+    input btnR,
     output reg hbridge1a,
     output reg hbridge2a,
     output reg hbridge1b,
     output reg hbridge2b
-
+  
 );
-// We can replace sw with motoiondir later
 reg [3:0] motiondir;
+
 // 0 is rest
 // 1 is forwards
 // 2 is backwards
@@ -16,44 +19,37 @@ reg [3:0] motiondir;
 // 4 is turn left
 // We can add more moves later
 always @(*) begin
-    case(sw)
-        0: 
-        begin
-            hbridge1a = 0;
-            hbridge1b = 0;
-            hbridge2a = 0;
-            hbridge2b = 0; 
-        end
-        1: 
-        begin
-            hbridge1a = 1;
-            hbridge1b = 0;
-            hbridge2a = 1;
-            hbridge2b = 0; 
-        end
-        2: 
-        begin
-            hbridge1a = 0;
-            hbridge1b = 1;
-            hbridge2a = 0;
-            hbridge2b = 1; 
-        end
-        3: 
-        begin
-            hbridge1a = 1;
-            hbridge1b = 0;
-            hbridge2a = 0;
-            hbridge2b = 1; 
-        end
-        4:
-        begin
-            hbridge1a = 0;
-            hbridge1b = 1;
-            hbridge2a = 1;
-            hbridge2b = 0; 
-        end
-    endcase
+    if (btnU == 1 || motiondir == 1 ) begin
+        hbridge1a = 1;
+        hbridge1b = 0;
+        hbridge2a = 1;
+        hbridge2b = 0;
+    end
+    else if (btnD == 1 || motiondir == 2) begin
+        hbridge1a = 0;
+        hbridge1b = 1;
+        hbridge2a = 0;
+        hbridge2b = 1; 
+    end
+    else if (btnL == 1 || motiondir == 3) begin
+        hbridge1a = 0;
+        hbridge1b = 1;
+        hbridge2a = 1;
+        hbridge2b = 0; 
+    end
+    else if (btnR == 1 || motiondir == 4) begin
+        hbridge1a = 1;
+        hbridge1b = 0;
+        hbridge2a = 0;
+        hbridge2b = 1; 
+    end
+    else begin
+        hbridge1a = 0;
+        hbridge1b = 0;
+        hbridge2a = 0;
+        hbridge2b = 0; 
+    end
 end
 
-endmodule
 
+endmodule
